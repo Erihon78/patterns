484066257cb6c300f3c1899738cc851947ea9aee
Oleg Rogov <oleg@heeeybro.com>
2017-12-25 19:45:33 +0300

    Case Switch

---
+++ js/pattern.js
@@ -0,0 +1,16 @@
+// console.log('It\'s Works!')
+// CASE SWITCH
+// Make many if more Simple
+var inspect = 1,
+	result = '';
+
+switch (inspect) {
+	case 0:
+		result = "zero";		
+		break;
+	case 1:
+		result = "one";
+		break;
+	default:
+		result: "unknown";
+}
